9309e81970304f0032934223e71b61642b4f4b5b
Chiranjivi keshav <117706982+chiranjivikeshav@users.noreply.github.com>
2022-11-08 15:17:46 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+double num1,num2;
+char op;
+cout<<"enter operator + - * / ^ "<<endl;
+cin>>op;
+cout<<"enter num1 and num2"<<endl;
+cin>>num1>>num2;
+switch (op){
+case'+':{
+    cout<<num1<<op<<num2<<"="<<num1 + num2<<endl;
+      break;
+}
+    
+  
+
+
+
+}
+    return 0;
+})
