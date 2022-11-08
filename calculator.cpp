412bdfa4f6be472505c4526e40b024fa759f3d17
Chiranjivi keshav <117706982+chiranjivikeshav@users.noreply.github.com>
2022-11-08 15:36:29 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -8,15 +8,32 @@ cin>>op;
 cout<<"enter num1 and num2"<<endl;
 cin>>num1>>num2;
 switch (op){
-case'+':{
-    cout<<num1<<op<<num2<<"="<<num1 + num2<<endl;
+case'+':
+        {cout<<num1<<op<<num2<<" = "<<num1 + num2<<endl;
       break;
 }
-    
-  
-
-
-
+ case'-':
+        {cout<<num1<<op<<num2<<" = "<<num1 - num2<<endl;
+      break;
+}
+ case'*':
+        {cout<<num1<<op<<num2 <<"= "<<num1 * num2<<endl;
+      break;
+}
+ case'/':
+        {if(num2==0){
+            cout<<"denominater is zero"<<endl;}
+     else{cout<<num1<<op<<num2<<" = "<<num1 / num2<<endl;
+      break;}
+}
+ case'^':{
+     int x=num1;
+     for(int i=1;i<num2;i++)
+        {
+         num1=num1*x;
+     }
+     cout<<x<< op <<num2<<" = "<<num1<<endl;}
+      break;
 }
     return 0;
-})
+}
